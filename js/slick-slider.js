63931969fba0bddf1d9f072fe7acbfd75c845893
Vladislav <eclipsik69@gmail.com>
2018-03-01 22:01:39 +0200

    added open story page

--- js/slick-slider.js
+++ js/slick-slider.js
@@ -8,4 +8,14 @@ $(document).ready(function(){
   	prevArrow: '<div class="slick-arrow slick-prev prev-ctrl"><i class="fa fa-lg fa-angle-left" aria-hidden="true"></i></div>',
 	nextArrow: '<div class="slick-arrow slick-next next-ctrl"><i class="fa fa-lg fa-angle-right" aria-hidden="true"></i></div>',  
   });
+
+  $('.attaches-content').slick({
+    infinite: true,
+  	slidesToShow: 4,
+  	slidesToScroll: 4,
+  	arrows: true,
+  	appendArrows: '.slider-control',
+  	prevArrow: '<div class="slick-arrow slick-prev prev-ctrl"><i class="fa fa-lg fa-angle-left" aria-hidden="true"></i></div>',
+	nextArrow: '<div class="slick-arrow slick-next next-ctrl"><i class="fa fa-lg fa-angle-right" aria-hidden="true"></i></div>',  
+  });
 });
